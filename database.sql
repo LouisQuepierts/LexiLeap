288026558d2d86d1384d0de07148bfb0ad949661
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-06-04 18:40:48 +0800

    update new database init script

---
+++ database.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS lexi_leap;
+CREATE DATABASE IF NOT EXISTS lexi_leap;
+USE lexi_leap;
+
+CREATE TABLE IF NOT EXISTS word (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    spell VARCHAR(100) NOT NULL UNIQUE,
+    definition_cn VARCHAR(64) NOT NULL,
+    definition_en VARCHAR(64) NOT NULL,
+    example_sentence VARCHAR(500) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    NAME VARCHAR(32) NOT NULL UNIQUE,
+    description VARCHAR(256) NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS affix (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    NAME VARCHAR(32) NOT NULL UNIQUE,
+    description VARCHAR(256) NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS word_tags (
+    word_id INT,
+    tag_id INT,
+    deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY (word_id, tag_id),
+    FOREIGN KEY (word_id) REFERENCES word(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
+
+CREATE TABLE IF NOT EXISTS word_affixes (
+    word_id INT,
+    affix_id INT,
+    deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY (word_id, affix_id),
+    FOREIGN KEY (word_id) REFERENCES word(id),
+    FOREIGN KEY (affix_id) REFERENCES affix(id)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    deleted BOOLEAN DEFAULT FALSE
+);
