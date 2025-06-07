436aad2a4c5a8f1ecac9792e5eb5f5d991f1b94f
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-06-07 11:37:59 +0800

    url_utils.js 修改url拼接格式

--- url_utils.js
+++ url_utils.js
@@ -1,5 +1,7 @@
 export const URL = "http://localhost/lexileap/";
+export const SUBFIX = ".php";
 
-export function _interface(module, intf) {
-    return URL + module + "/php/" + intf + ".php";
+
+export function _interface(module, _interface) {
+    return URL + module + "/interface/" + _interface + SUBFIX;
 }
