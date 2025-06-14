55170dca7c51b60fccd45b22d420409cfe740379
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-06-15 00:25:54 +0800

    修改部分改名后遗漏的内容

--- general/js/practice.js
+++ general/js/practice.js
@@ -4,8 +4,8 @@ import {QuestionController, QuestionHistory} from "./exercise/QuestionController
 const url = "/LexiLeap/general/view/practice/"
 
 const question_types = [
-    // "spelling",
-    // "multiple-choice",
+    "spelling",
+    "multiple-choice",
     "matching",
 ]
 
