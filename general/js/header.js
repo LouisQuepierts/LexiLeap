6259a27bc954d95cde8e6fdfc233db374a67ceea
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-06-15 16:05:21 +0800

    移除 header.js 中无用的代码

--- general/js/header.js
+++ general/js/header.js
@@ -14,16 +14,6 @@ const header_config = {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // 跳转
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-        });
-    });
-
     const header = document.getElementById("main-header");
     const header_nav = document.createElement("div");
     header_nav.classList.add("header-nav");
